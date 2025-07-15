341c4ee8da78169f162e11c30bf456a3367c7929
fozia <foziakassa19@gmail.com>
2025-07-15 16:43:38 +0300

    chenge

--- src/app/components/projects/ProjectLayout.tsx
+++ src/app/components/projects/ProjectLayout.tsx
@@ -37,8 +37,8 @@ const ProjectLayout = (props: ProductProps) => {
       <div className="flex items-center justify-center space-x-2 
       ">
         <h2 className="text-gray-50 text-xl">{ProductData?.title}</h2>
-        <p className="text-gray-200
-         hidden sm:inline-block text-lg">
+        <p className="text-gray-300
+         hidden sm:inline-block text-sm">
           {ProductData?.description}</p>
       </div>
       {/* <div className="self-end flex-1 mx-2 mb-1 bg-transparent 
