1e84c7b8db4ebe4cb518eecb6417de9f167cbf24
foziakassa <foziakassa19@gmail.com>
2026-04-28 16:32:10 +0300

    updated

--- src/app/components/about/experiance.tsx
+++ src/app/components/about/experiance.tsx
@@ -61,7 +61,7 @@ export function Expriance3(){
           text-xl sm:text-sm md:text-lg text-gray-200 ">
             {/* {data.data[1].description} */}
             Frontend Developer & Mobile App Developer
-import 'package:emebet/bloc/edit_bloc/edit_bloc.dart';
+
 Company: Vintage Technologies
 Technologies Used: React.js, Flutter.js
             </sub>
